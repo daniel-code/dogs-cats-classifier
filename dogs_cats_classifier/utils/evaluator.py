fd431be6ef03adcec12874d56b60033f0ac09ae7
Su Yan-Ru <daniel78761215@gmail.com>
2022-11-01 21:57:19 +0800

    chore: add comments

--- dogs_cats_classifier/utils/evaluator.py
+++ dogs_cats_classifier/utils/evaluator.py
@@ -1,6 +1,7 @@
 import os
 
 import numpy as np
+import pandas as pd
 import seaborn as sns
 import torch
 from PIL import Image
@@ -9,9 +10,18 @@ from sklearn.metrics import roc_curve, confusion_matrix, auc, accuracy_score, pr
     roc_auc_score
 from tqdm import tqdm
 
+from torch.utils.data import DataLoader
+from pytorch_lightning import LightningModule
+
 
 class Evaluator:
-    def __init__(self, model, output_path):
+    def __init__(self, model: LightningModule, output_path):
+        """
+
+        Args:
+            model: model to be evaluated.
+            output_path: path to save the figure.
+        """
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
         self.model = model
@@ -20,7 +30,17 @@ class Evaluator:
 
         self.output_path = output_path
 
-    def inference_dataloader(self, dataloader):
+    def inference_dataloader(self, dataloader: DataLoader) -> (np.ndarray, np.ndarray, np.ndarray):
+        """
+
+        Args:
+            dataloader: torch DataLoader.
+
+        Returns:
+            ground_truth: Ground truth (correct) labels.
+            predictions: Predicted labels, as returned by a classifier.
+            class_predictions: label class. 0 for cats and 1 for dogs.
+        """
         predictions = []
         ground_truth = []
 
@@ -42,7 +62,19 @@ class Evaluator:
 
         return ground_truth, predictions, class_predictions
 
-    def evaluate(self, dataloader, title, verbose=False):
+    def evaluate(self, dataloader: DataLoader, title, verbose=False) -> (np.ndarray, np.ndarray, np.ndarray):
+        """
+        evaluate model.
+        Args:
+            dataloader: torch DataLoader.
+            title: title of figure and filename.
+            verbose: display figure or not. Default: False.
+
+        Returns:
+            ground_truth: Ground truth (correct) labels.
+            predictions: Predicted labels, as returned by a classifier.
+            class_predictions: label class. 0 for cats and 1 for dogs.
+        """
         ground_truth, predictions, class_predictions = self.inference_dataloader(dataloader=dataloader)
         print('Title:', title)
         print('Accuracy:', accuracy_score(ground_truth, class_predictions))
@@ -59,6 +91,16 @@ class Evaluator:
         return ground_truth, predictions, class_predictions
 
     def plot_auc_confusion_matrix(self, y_ture: np.ndarray, y_pred: np.ndarray, title: str, output_path, verbose=False):
+        """
+        plot auc roc curve and confusion matrix.
+        Args:
+            y_ture: Ground truth (correct) labels.
+            y_pred: Predicted labels, as returned by a classifier.
+            title: name for figure title and file name.
+            output_path: Path to save figure.
+            verbose: Display figure or not. Default: False.
+
+        """
         y_class = (y_pred > 0.5).astype(int)
 
         fig, axs = plt.subplots(1, 2, figsize=(10, 5))
@@ -88,7 +130,18 @@ class Evaluator:
         if verbose:
             plt.show()
 
-    def plot_images(self, df, title, output_path, n_cols=10, verbose=False):
+    def plot_images(self, df: pd.DataFrame, title: str, output_path, n_cols=10, verbose=False):
+        """
+        plot images of Dataframe.
+        Args:
+            df:
+            title: name for figure title and file name.
+            output_path: Path to save figure.
+            n_cols: Number of columns.
+            verbose: Display figure or not. Default: False.
+
+        """
+        assert 'filename' in df.columns and 'ground_truth' in df.columns, f'Dataframe should contain `filename` and `ground_truth`. Received: {df.columns}'
         nrows = len(df.index) // n_cols + 1
         fig, axs = plt.subplots(nrows=nrows, ncols=n_cols, figsize=(n_cols, int(nrows * 2.5)))
 
