8cc4685bcd50886722358c46015ca97d4b311885
Su Yan-Ru <daniel78761215@gmail.com>
2022-11-03 17:12:51 +0800

    chore: upgrade version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='dogs_cats_classifier',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.1.1',
     description='Create an algorithm to distinguish dogs from cats',
     author='YanRu',
     license="MIT",
